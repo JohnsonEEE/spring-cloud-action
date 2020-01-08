bf5fe8e9b1e0e3e70312fe87799e8d8d01db7dc1
yyi <yiyi@thsupcom.com>
2020-01-08 10:16:31 +0800

    create demo, including 2 eureka servers, 3 eureka clients

---
+++ hello-service-2/src/main/java/org/yiyi/hello/HelloController.java
@@ -0,0 +1,63 @@
+/*
+ *
+ *
+ * Copyright ( c ) 2020 TH Supcom Corporation. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of TH Supcom
+ * Corporation ("Confidential Information").  You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with TH Supcom Corporation or a TH Supcom
+ * authorized reseller (the "License Agreement"). TH Supcom may make changes to the
+ * Confidential Information from time to time. Such Confidential Information may
+ * contain errors.
+ *
+ * EXCEPT AS EXPLICITLY SET FORTH IN THE LICENSE AGREEMENT, TH Supcom DISCLAIMS ALL
+ * WARRANTIES, COVENANTS, REPRESENTATIONS, INDEMNITIES, AND GUARANTEES WITH
+ * RESPECT TO SOFTWARE AND DOCUMENTATION, WHETHER EXPRESS OR IMPLIED, WRITTEN OR
+ * ORAL, STATUTORY OR OTHERWISE INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A
+ * PARTICULAR PURPOSE. TH Supcom DOES NOT WARRANT THAT END USER'S USE OF THE
+ * SOFTWARE WILL BE UNINTERRUPTED, ERROR FREE OR SECURE.
+ *
+ * TH Supcom SHALL NOT BE LIABLE TO END USER, OR ANY OTHER PERSON, CORPORATION OR
+ * ENTITY FOR INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL
+ * DAMAGES, OR DAMAGES FOR LOSS OF PROFITS, REVENUE, DATA OR USE, WHETHER IN AN
+ * ACTION IN CONTRACT, TORT OR OTHERWISE, EVEN IF TH Supcom HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES. TH Supcom' TOTAL LIABILITY TO END USER SHALL NOT
+ * EXCEED THE AMOUNTS PAID FOR THE TH Supcom SOFTWARE BY END USER DURING THE PRIOR
+ * TWELVE (12) MONTHS FROM THE DATE IN WHICH THE CLAIM AROSE.  BECAUSE SOME
+ * STATES OR JURISDICTIONS DO NOT ALLOW LIMITATION OR EXCLUSION OF CONSEQUENTIAL
+ * OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO END USER.
+ *
+ * Copyright version 2.0
+ */
+package org.yiyi.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author yi.yi
+ * @date 2020.01.07
+ */
+@RestController
+public class HelloController
+{
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello ()
+    {
+        StringBuilder sb = new StringBuilder ();
+        List <ServiceInstance> instances = client.getInstances ("HELLO-SERVICE");
+        instances.forEach (i -> sb.append (i.getInstanceId ()).append (" from service 222"));
+        return sb.toString ();
+    }
+}
